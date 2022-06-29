1677d05a0ca89e91e1c8c38c5f8e5d63329b8533
Anton Pomogaev <antonpomogaev@gmail.com>
2022-06-29 20:15:08 +0300

    feat: add types to controller/appLoader.js

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '48310286a8194e7e8c9cafd19c8d81b7',
         });
     }
 }
