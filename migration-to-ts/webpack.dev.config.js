e8c1e03da8f828ee874f1d14a73ae6e546b7b803
Anton Pomogaev <antonpomogaev@gmail.com>
2022-06-29 13:51:36 +0300

    feat: add sass, typescript and eslint support

--- migration-to-ts/webpack.dev.config.js
+++ migration-to-ts/webpack.dev.config.js
@@ -4,6 +4,12 @@ module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: path.resolve(__dirname, '../dist'),
+        static: {
+            directory: path.join(__dirname, '../dist'),
+        },
+        open: true,
+        compress: true,
+        port: 9000,
+        hot: true,
     },
 };
