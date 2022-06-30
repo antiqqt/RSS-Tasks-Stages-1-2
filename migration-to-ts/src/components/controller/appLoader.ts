6a7cde7c66eb34bd03f6e538da4f1a78cb1585d0
Anton Pomogaev <antonpomogaev@gmail.com>
2022-06-30 15:27:41 +0300

    fix: change original api link to allow calls to newsapi on gh-pages

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '48310286a8194e7e8c9cafd19c8d81b7',
         });
     }
